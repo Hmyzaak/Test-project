9163793b10c71d995decdc07ebe884ed7494652e
JaVoJ <davias@email.cz>
2026-02-27 17:05:24 +0100

    final calculator - ready for final ui

--- Profit_calculator/retirement.py
+++ Profit_calculator/retirement.py
@@ -19,34 +19,48 @@ def simulate_withdrawal_until_depleted(
     initial_capital: Decimal,
     withdrawal_amount: Decimal,
     withdrawals_per_year: int,
+    annual_inflation_rate: Decimal,
     context: InvestmentContext,
     fee_amount: Decimal = Decimal(0),
     fee_type: FeeType = FeeType.FIXED,
 ) -> List[WithdrawalPeriod]:
 
+    # reálný kapitál - kupní síla naspořených financí
     capital = initial_capital
-    periodic_rate = context.calculate_periodic_interest_rate()
+    # úroková sazba pro kapitalizační periodu
+    periodic_compounding_interest_rate = context.calculate_periodic_interest_rate()
+    # počet period pro úročení ku periodě výběru
+    compounding_periods_per_withdrawal_period = context.compounding_per_year / withdrawals_per_year
+    # poměrná část roční inflace (sazba roční inflace je efektivní) ku periodě výběru
+    periodic_inflation_rate = (Decimal(1) + annual_inflation_rate) ** (Decimal(1) / Decimal(withdrawals_per_year)) - Decimal(1)
+    # vybíraný obnos zvýšený o poměrnou část roční inflace ku periodě výběru (aby byla vybírána částka o stále stejné kupní síle)
+    updated_withdrawal_amount = withdrawal_amount
 
+    # výsledky simulace
     results = []
-    period = 0
+    # počet provedených výběrů
+    withdrawal_periods_done = 0
 
     while capital > 0:
-        period += 1
-        year = (period - 1) // withdrawals_per_year + 1
-
-        capital_start = capital
+        withdrawal_periods_done += 1
+        year = (withdrawal_periods_done - 1) // withdrawals_per_year + 1
 
         # úročení
-        capital *= (Decimal(1) + periodic_rate)
+        capital_start = capital
+        capital *= ((Decimal(1) + Decimal(periodic_compounding_interest_rate)) ** Decimal(compounding_periods_per_withdrawal_period))
+        capital /= (Decimal(1) + periodic_inflation_rate)
+        capital_before_withdrawal = capital
+        updated_withdrawal_amount *= (Decimal(1) + periodic_inflation_rate)
+
         # poplatek
         if fee_type == FeeType.PROPORTIONAL:
-            fee = withdrawal_amount * fee_amount
+            fee = updated_withdrawal_amount * fee_amount
         else:
             fee = fee_amount
 
-        total_withdrawal = withdrawal_amount + fee
+        total_withdrawal = updated_withdrawal_amount + fee
 
-        # pokud by poslední výběr překročil kapitál, vybere se jen to, co zbývá (poplatek se ruší)
+        # pokud by poslední výběr překročil kapitál, vybere se jen to, co zbývá (poplatek se přepisuje, aby smyčka nepokračovala do záporu)
         if total_withdrawal > capital:
             total_withdrawal = capital
             fee = Decimal(0)
@@ -55,10 +69,10 @@ def simulate_withdrawal_until_depleted(
 
         results.append(
             WithdrawalPeriod(
-                period=period,
+                period=withdrawal_periods_done,
                 year=year,
                 capital_start=capital_start,
-                withdrawal=withdrawal_amount,
+                withdrawal=updated_withdrawal_amount,
                 fee=fee,
                 capital_end=capital,
             )
@@ -67,5 +81,9 @@ def simulate_withdrawal_until_depleted(
         if capital <= 0:
             break
         # Zbytečné, ale pojistka pro while cyklus
+        if capital > capital_start:
+            loop_amount = int(capital_before_withdrawal - capital)
+            print(f"Nekonečný cyklus generující stálou rentu. Následující věta nezobrazuje pravdivý počet let, ale to, že již v roce 1 bylo dosaženo podmínky pro nekonečnou smyčku - perioda určená pro výběr renty generuje příjem {loop_amount} CZK.")
+            break
 
     return results
--- Profit_calculator/ui.py
+++ Profit_calculator/ui.py
@@ -2,12 +2,13 @@ from models import *
 from projections import generate_yearly_projection
 from retirement import simulate_withdrawal_until_depleted
 
+annual_inflation_rate = Decimal("0.02")
 
-snp_etf = InvestmentContext(18, 0.08, CompoundingFrequency.PER_DIEM, 0.0003, InterestType.EFFECTIVE, 0.0)
+snp_etf = InvestmentContext(18, 0.10, CompoundingFrequency.PER_DIEM, 0.0003, InterestType.EFFECTIVE, 0)
 vstupni_vklad = Deposit(DepositType.ENTRY, 0, 0, 0.0015, FeeType.PROPORTIONAL, AnnuityType.DUE)
-periodicky_vklad = Deposit(DepositType.PERIODIC, 2000, 12, 0.0015, FeeType.PROPORTIONAL, AnnuityType.DUE)
+periodicky_vklad = Deposit(DepositType.PERIODIC, 12000, 12, 0.0015, FeeType.PROPORTIONAL, AnnuityType.DUE)
 
-projections = generate_yearly_projection(snp_etf.years, snp_etf, [vstupni_vklad, periodicky_vklad], Decimal(0.02),)
+projections = generate_yearly_projection(snp_etf.years, snp_etf, [vstupni_vklad, periodicky_vklad], annual_inflation_rate,)
 for p in projections:
       print(f'Pro rok {p.year} vloženo {int(p.total_deposited)} CZK a naspořeno {int(p.total_value_nominal)} CZK - reálná kupní síla: {int(p.total_value_real)} CZK.')
 
@@ -24,13 +25,13 @@ for p in projections:
       print(f'Pro rok {p.year} vloženo {int(p.total_deposited)} CZK a naspořeno {int(p.total_value_nominal)} CZK - reálná kupní síla: {int(p.total_value_real)} CZK.')
 """
 
-"""
+withdrawal_amount = Decimal("20000")
 real_capital = projections[-1].total_value_real
 withdrawal_simulation = simulate_withdrawal_until_depleted(
       initial_capital=real_capital,
-      withdrawal_amount=Decimal(20000),
+      withdrawal_amount=withdrawal_amount,
       withdrawals_per_year=12,
-      context=investice,
+      annual_inflation_rate=annual_inflation_rate,
+      context=snp_etf,
 )
-print(f'Kapitál vydrží {withdrawal_simulation[-1].year} let při výběru {withdrawal_simulation[0].withdrawal} CZK.')
-"""
+print(f'Kapitál vydrží {withdrawal_simulation[-1].year} let při výběru {withdrawal_amount} CZK.')
